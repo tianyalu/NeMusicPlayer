7e3156ce5b2c6d1447e16317e151b5b76a8ad4d9
tianyalu <styzf@sina.com>
2019-11-06 00:28:27 +0800

    push

---
+++ app/src/main/java/com/sty/ne/music/player/view/DiscView.java
@@ -0,0 +1,19 @@
+package com.sty.ne.music.player.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class DiscView extends RelativeLayout {
+    public DiscView(Context context) {
+        this(context, null);
+    }
+
+    public DiscView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DiscView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
